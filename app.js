ac739a9e28172bd88f66aee437f399701030e62e
just-fontaine <henrypmesquita@gmail.com>
2025-01-20 18:21:50 -0300

    Update app.js
    Alguns ponto e virgula ajustados.

--- app.js
+++ app.js
@@ -4,31 +4,30 @@
 let nomes_amigos = [];
 let botao_adicionar = document.getElementById('button-add');
 let lista_amigos = document.querySelector('#listaAmigos');
-
-let resultado = document.querySelector('#resultado')
+let resultado = document.querySelector('#resultado');
 
 function adicionarAmigo() {
     let input = document.getElementById('amigo').value;
     
     if (input == '') {
-        alert('O nome não pode estar vazio.')
+        alert('O nome não pode estar vazio.');
     }
     else {
         if (nomes_amigos.includes(input)) {
-            alert('O nome não pode ser repetido.')
+            alert('O nome não pode ser repetido.');
         }
         else {
             nomes_amigos.push(input.toLowerCase());
             console.log(nomes_amigos);
         
-            lista_amigos.innerHTML += `<li>${input}</li>`
+            lista_amigos.innerHTML += `<li>${input}</li>`;
         }
     }
 }
 
 function sortearAmigo() {
     let indiceAleatorio = Math.floor(Math.random() * nomes_amigos.length);
-    let nomeEscolhido = nomes_amigos[indiceAleatorio]
+    let nomeEscolhido = nomes_amigos[indiceAleatorio];
 
-    resultado.innerHTML = `Nome escolhido: ${nomeEscolhido}`
+    resultado.innerHTML = `Nome escolhido: ${nomeEscolhido}`;
 }
